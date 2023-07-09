8cc91607a2928df0238b214aba6630c339d198e6
Roman Komarov <kizmarh@ya.ru>
2023-07-09 21:36:54 +0200

    Attempt to put full text content in RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,21 @@
 import type { APIContext } from 'astro';
-import rss from '@astrojs/rss';
+import rss, { RSSOptions } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { betterSlug } from '../lib/betterSlug';
+import { markdown } from '@astropub/md'
 
 export async function get(context: APIContext) {
 	const posts = await getCollection('blog');
+	const items: RSSOptions['items'] = [];
+	for (const post of posts) {
+		const content = (await markdown(post.body)).toString();
+		items.push({
+			...post.data,
+			link: `/${betterSlug(post.slug)}/`,
+			content,
+		});
+	}
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
@@ -15,9 +25,6 @@ export async function get(context: APIContext) {
 			<language>en</language>
 			<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />
 		`,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${betterSlug(post.slug)}/`,
-		})),
+		items,
 	});
 }
