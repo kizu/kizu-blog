957d77f386b2406bb0bd9f9ea51bcbf91db0eca7
Roman Komarov <kizmarh@ya.ru>
2023-10-22 23:26:50 +0200

    Fix mdx for rss

--- src/lib/sanitizeMDX.ts
+++ src/lib/sanitizeMDX.ts
@@ -1,6 +1,6 @@
 export const sanitizeMDX = (code: string, link: string) => {
   return code
-    .replaceAll(/import .+\n/g, '')
+    .replaceAll(/(import|export) .+\n/g, '')
     .replaceAll(/<[A-Z][^>]*\/?>/g, `[Component is not available in RSS, visit the the site to access it.](${link})`)
     .replaceAll(/<\/[A-Z][^>]*>/g, '');
 };
