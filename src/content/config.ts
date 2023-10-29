e8b083e23dded6c76b64161ab9737a428d6c552c
Roman Komarov <kizmarh@ya.ru>
2023-10-29 12:50:12 +0100

    Add microformats

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,6 +16,7 @@ const blog = defineCollection({
 			.transform((str) => (str ? new Date(str) : undefined)),
 		heroImage: z.string().optional(),
 		mastodonPostId: z.string().optional(),
+		tags: z.array(z.string()),
 	}),
 });
 
