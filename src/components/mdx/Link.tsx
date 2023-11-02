1762ef59221932d7952b37e1131d9b8e367ef4ff
Roman Komarov <kizmarh@ya.ru>
2023-11-02 20:25:15 +0100

    Shadow DOM on Regular Elements

--- src/components/mdx/Link.tsx
+++ src/components/mdx/Link.tsx
@@ -3,7 +3,7 @@ export const Link = ({ children, ...props }) => {
 	// If the text content of the link starts and ends with the quotes,
 	// wrap the contens with a `<u>` to enable a better link display.
 	const modifiedContent =
-		children.props.value.toString().replace(/^“(.+)”$/, '“<u>$1</u>”');
+		children.props.value.toString().replace(/^([“‘])(.+)([”’])$/, '$1<u>$2</u>$3');
 	return (
 		<a
 			{...props}
