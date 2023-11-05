392bab01f84cfeea86311f9df5e410e62510e41f
Roman Komarov <kizmarh@ya.ru>
2023-11-05 13:13:23 +0100

    Do not use days as folders

--- src/lib/betterSlug.ts
+++ src/lib/betterSlug.ts
@@ -1,1 +1,7 @@
-export const betterSlug = (slug: string) => slug.replace(/^(\d+\/)+/, '');
+export const betterSlug =
+	(slug: string) =>
+		slug
+			// Do not include folders that are only numbers.
+			.replace(/^(\d+\/)+/, '')
+			// When a page is nested into a named folder, do not output duplicate name.
+			.replace(/^([^\/]+)\/\1/, '$1');
