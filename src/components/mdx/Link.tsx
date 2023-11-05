05f639f55d384f495af9fbc973d462ed46a325c9
Roman Komarov <kizmarh@ya.ru>
2023-11-05 15:37:52 +0100

    Improve design

--- src/components/mdx/Link.tsx
+++ src/components/mdx/Link.tsx
@@ -1,8 +1,15 @@
+import type { ComponentChildren } from 'preact';
 
-export const Link = ({ children, ...props }) => {
+interface LinkProps {
+	children: ComponentChildren;
+}
+
+export const Link = ({ children, ...props }: LinkProps) => {
 	// If the text content of the link starts and ends with the quotes,
 	// wrap the contens with a `<u>` to enable a better link display.
 	const modifiedContent =
+		// Need to explore a better way to do it.
+		// @ts-expect-error
 		children.props.value.toString().replace(/^([“‘])(.+)([”’])$/, '$1<u>$2</u>$3');
 	return (
 		<a
--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@ export const AUTHOR = 'Roma Komarov';
 export const MAIN_URL = 'https://kizu.dev';
 export const MASTODON_URL = 'https://front-end.social/@kizu';
 
-export const SITE_TITLE = 'Random Notes from kizu.dev';
+export const SITE_TITLE = 'Roma’s Unpolished Posts';
 export const SITE_DESCRIPTION = 'Roma’s personal blog';
