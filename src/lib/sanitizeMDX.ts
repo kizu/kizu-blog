a06a6cc30372a1760917427a7795ddafa9491123
Roman Komarov <kizmarh@ya.ru>
2023-11-14 18:00:58 +0100

    Better RSS sanitize

--- src/lib/sanitizeMDX.ts
+++ src/lib/sanitizeMDX.ts
@@ -1,7 +1,15 @@
 export const sanitizeMDX = (code: string, link: string) => {
-  return code
-    .replaceAll(/(import|export) .+\n/g, '')
-    .replaceAll(/<[A-Z][^>]*\/?>/g, `[Component is not available in RSS, visit the the site to access it.](${link})`)
-    .replaceAll(/!\[([^\]]+)\]\([^\)]+\)/g, `[“$1” (image is not available in RSS, visit the the site to access it.)](${link})`)
-    .replaceAll(/<\/[A-Z][^>]*>/g, '');
+	const codeSplit = code.split(/(?=```)/);
+	let result = '';
+	for (const part of codeSplit) {
+		if (part.match(/^```\w/)) {
+			result += part;
+			continue;
+		}
+		result += part.replaceAll(/(import|export) .+\n/g, '')
+			.replaceAll(/<[A-Z][^>]*\/?>/g, `[Component is not available in RSS, visit the the site to access it.](${link})`)
+			.replaceAll(/!\[([^\]]+)\]\([^\)]+\)/g, `[“$1” (image is not available in RSS, visit the the site to access it.)](${link})`)
+			.replaceAll(/<\/[A-Z][^>]*>/g, '');
+	}
+	return result;
 };
