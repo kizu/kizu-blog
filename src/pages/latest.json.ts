cd7afe8d6564348cebcf80873151bc5d026731e6
Roman Komarov <kizmarh@ya.ru>
2023-12-05 22:59:30 +0100

    More relevant latest posts for the main site

--- src/pages/latest.json.ts
+++ src/pages/latest.json.ts
@@ -6,9 +6,18 @@ interface Item {
 	url: string;
 }
 export async function get(context: APIContext) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('blog')).sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+	);
 	const items: Item[] = [];
 	for (const post of posts) {
+		// Do not use weekly or non-CSS/HTML posts for the latest.
+		if (
+			post.data.tags.includes('weekly') ||
+			!post.data.tags.some(tag => ['CSS', 'HTML'].includes(tag))
+		) {
+			continue;
+		}
 		const url = `${context.site}${betterSlug(post.slug)}/`;
 		items.push({
 			...post.data,
