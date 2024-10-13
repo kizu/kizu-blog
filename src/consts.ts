f0ff485ff0eecec56947103d6ba95d25e4838d4c
Roman Komarov <kizmarh@ya.ru>
2024-10-13 11:23:34 +0200

    Add author meta

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 export const AUTHOR = 'Roma Komarov';
 export const MAIN_URL = 'https://kizu.dev';
 export const MASTODON_URL = 'https://front-end.social/@kizu';
+export const MASTODON_HANDLE = '@kizu@front-end.social';
 
 export const SITE_TITLE = 'Roma’s Unpolished Posts';
 export const SITE_DESCRIPTION = 'Roma’s personal blog';
